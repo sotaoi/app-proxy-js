1238c60fdd438227e7d55267e4bd7149e1d362fb
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 18:35:15 +0300

    refactor: update

--- scripts/routines/bootstrap-routine.js
+++ scripts/routines/bootstrap-routine.js
@@ -1,13 +1,17 @@
 #!/bin/env node
 
+process.env.NODE_ENV = 'production';
 const { init } = require('@app/omni/init');
 init();
+const { config } = require('@app/omni/config');
+const { AppKernel } = require('@sotaoi/api/app-kernel');
 const { getAppInfo } = require('@sotaoi/omni/get-app-info');
 const { Store } = require('@sotaoi/api/store');
 const { sconnect } = require('@sotaoi/api/db');
 const { Model } = require('@sotaoi/api/db/model');
 
 const bootstrapRoutine = async () => {
+  new AppKernel().bootstrap(config);
   await Store.init(getAppInfo(), {}, {}, null);
   await sconnect();
   return async () => {
@@ -15,4 +19,4 @@ const bootstrapRoutine = async () => {
   };
 };
 
-export { bootstrapRoutine };
+module.exports = { bootstrapRoutine };
